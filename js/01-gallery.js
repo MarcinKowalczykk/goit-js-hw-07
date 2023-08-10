1364e0c20ab8d30c6568751f7a098366d17bd078
Marcin Kowalczyk <marcinck80@gmail.com>
2023-08-10 23:06:50 +0200

    lightbox added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-galleryItems.forEach(item =>{
-    const galleryItem = document.createElement('div');
-    galleryItem.classList.add('gallery__item')
 
-    const galleryLink = document.createElement('a');
-    galleryLink.href = item.preview;
-    galleryLink.classList.add('gallery__link');
-    
-    const galleryImage = document.createElement('img');
-    galleryImage.src = item.preview;
-    galleryImage.dataset.src = item.original;
-    galleryImage.alt = item.description;
-    galleryImage.classList.add('gallery__image');
-    
-    galleryLink.appendChild(galleryImage);
-    galleryItem.appendChild(galleryLink);
-    gallery.appendChild(galleryItem);
+galleryItems.forEach(item => {
+  const galleryitem = document.createElement('div');
+  galleryitem.classList.add('gallery__item');
+
+  const gallerylink = document.createElement('a');
+  gallerylink.href = item.original;
+  gallerylink.classList.add('gallery__link', 'lightbox');
+
+  const galleryimage = document.createElement('img');
+  galleryimage.src = item.preview;
+  galleryimage.dataset.src = item.original;
+  galleryimage.alt = item.description;
+  galleryimage.classList.add('gallery__image');
+
+  gallerylink.appendChild(galleryimage);
+  galleryitem.appendChild(gallerylink);
+  gallery.appendChild(galleryitem);
+});
+
+const galleryLinks = document.querySelectorAll('.lightbox');
+
+galleryLinks.forEach(galleryLink => {
+  galleryLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    const imageSource = this.href;
+    const instance = basicLightbox.create(`<img src="${imageSource}">`);
+    instance.show();
+  });
 });
 
 console.log(galleryItems);
